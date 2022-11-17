0ea51e1eac77f759262b16317d357ffef102274f
uchmielewska <u.chmielewska@millionmonkeyssoftware.com>
2022-11-17 11:42:32 +0100

    init frontend

---
+++ frontend/src/components/AppLayout/index.js
@@ -0,0 +1,11 @@
+import { Flex, Heading } from "@chakra-ui/react";
+
+const AppLayout = () => {
+  return (
+    <Flex bg="teal" h="100vw" justifyContent="center">
+      <Heading color="white">Laser Shooting Game</Heading>
+    </Flex>
+  );
+};
+
+export default AppLayout;
---
+++ frontend/src/services/api/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const apiBase = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+const api = {
+  recipes: () => apiBase.get("/recipes"),
+  deleteRecipe: ({ id }) => apiBase.delete("/recipes", { data: { id } }),
+  recipe: ({ id }) => apiBase.get(`/recipes/${id}`),
+  addRecipe: ({ recipeName, recipeBody, ingredients }) =>
+    apiBase.post("/recipes", {
+      recipeName,
+      recipeBody,
+      ingredients,
+    }),
+  editRecipe: ({ recipeId, recipeName, recipeBody, ingredients }) =>
+    apiBase.put("/recipes", {
+      recipeId,
+      recipeName,
+      recipeBody,
+      ingredients,
+    }),
+};
+
+export default api;
